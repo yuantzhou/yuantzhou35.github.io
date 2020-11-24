5be46ca188f351f2cd3d384aa902a298d232c7e5
yuantzhou <46725037+yuantzhou@users.noreply.github.com>
2020-11-24 00:46:35 -0700

    Update a1.js

--- a1.js
+++ a1.js
@@ -25,9 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
     listI.addEventListener('click',function(){
              //display for each garlley
       document.querySelector('div.loader').style.display= "block";
-        let Gallery = `https:www.randyconnolly.com/funwebdev/3rd/api/art/paintings.php?galley=${li}`;
-      
-        fetch(Gallery).then((res)=> res.json()).then(g => {listGallery(g)}).catch(error => console.error(error));
+ fetch(`https:www.randyconnolly.com/funwebdev/3rd/api/art/paintings.php?galley=${li}`).then((res)=> res.json()).then(g => {listGallery(g)}).catch(error => console.error(error));
         function listGallery(g){
         let A= document.querySelector('div.a section');
            A.style.display = "inline-block";
