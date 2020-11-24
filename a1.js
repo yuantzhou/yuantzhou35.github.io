c345d27b0b17323baa2943fcb1c141271c5bf02c
yuantzhou <46725037+yuantzhou@users.noreply.github.com>
2020-11-23 20:28:12 -0700

    Update a1.js
    Chang line 30 for  variable

--- a1.js
+++ a1.js
@@ -25,8 +25,9 @@ document.addEventListener("DOMContentLoaded", function() {
     listI.addEventListener('click',function(e){
              //display for each garlley
         console.log(e.target.innerHTML);
+     let tar =e.target.innerHTML;
       document.querySelector('div.loader').style.display= "block";
-        let Gallery = "https:www.randyconnolly.com/funwebdev/3rd/api/art/paintings.php?galley= `${e.target.innerHTML}`";
+        let Gallery = "https:www.randyconnolly.com/funwebdev/3rd/api/art/paintings.php?galley= `${tar}";
       fetch(Gallery).then((resp)=> resp.json()).then(g => {listGallery(g)}).catch(error => console.error(error));
         function listGallery(g){
         let A= document.querySelector('div.a section');
